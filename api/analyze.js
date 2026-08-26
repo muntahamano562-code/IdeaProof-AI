e6b38c9473d0c9c860a6b673f8d115e9e9c738a6
muntahamano562-code <muntahamano562@gmail.com>
2026-08-26 21:07:33 +0500

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-import { analyzeHandler } from './analyzeCore'
+import { analyzeHandler } from './analyzeCore.js'
 
 export default async function handler(req, res) {
   await analyzeHandler(req, res)
